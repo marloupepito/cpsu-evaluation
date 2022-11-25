401983077957db16f07462f8cbe5ddd8d57b99a9
marloupepito <marloupepito07@gmail.com>
2022-11-25 21:43:49 +0800

    route changes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,7 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
-
+ protected $namespace = 'App\\Http\\Controllers';
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
      *
@@ -26,14 +26,16 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->configureRateLimiting();
+         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
+            Route::prefix('api')
+                ->middleware('api')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use App\http\Controllers\EvaluatorController;
+use App\http\Controllers\FacultyController;
+use App\http\Controllers\QuestionaireController;
+use App\http\Controllers\ResultsController;
+use App\http\Controllers\ScheduleController;
+use App\http\Controllers\UsersController;
+use App\http\Controllers\SubjectLoadingController;
+use App\http\Controllers\StudentSubjectLoadingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,47 +38,47 @@ Route::get('/{vue?}',function(){
 
 
 
-Route::post('/logout','App\http\Controllers\UsersController@logout');//check
-Route::post('/get_admin','App\http\Controllers\UsersController@get_admin');
-Route::post('/get_evaluators','App\http\Controllers\EvaluatorController@get_evaluators'); //check
-Route::post('/logout_evaluator','App\http\Controllers\EvaluatorController@logout_evaluator');
+Route::post('/logout','UsersController@logout');//check
+Route::post('/get_admin','UsersController@get_admin');
+Route::post('/get_evaluators','EvaluatorController@get_evaluators'); //check
+Route::post('/logout_evaluator','EvaluatorController@logout_evaluator');
 
-Route::post('/get_faculty','App\http\Controllers\FacultyController@get_faculty');//check
-Route::post('/get_all_faculty','App\http\Controllers\FacultyController@get_all_faculty');
-Route::post('/add_faculty','App\http\Controllers\FacultyController@add_faculty');//check
+Route::post('/get_faculty','FacultyController@get_faculty');//check
+Route::post('/get_all_faculty','FacultyController@get_all_faculty');
+Route::post('/add_faculty','FacultyController@add_faculty');//check
 
-Route::post('/scan_qrcode','App\http\Controllers\EvaluatorController@scan_qrcode');
-Route::post('/evaluator_session','App\http\Controllers\EvaluatorController@evaluator_session');
-Route::post('/add_student','App\http\Controllers\EvaluatorController@add_student');
+Route::post('/scan_qrcode','EvaluatorController@scan_qrcode');
+Route::post('/evaluator_session','EvaluatorController@evaluator_session');
+Route::post('/add_student','EvaluatorController@add_student');
 
-Route::post('/get_questionaire','App\http\Controllers\QuestionaireController@get_questionaire');
-Route::put('/edit_questionaire1','App\http\Controllers\QuestionaireController@edit_questionaire1');
-Route::put('/edit_questionaire2','App\http\Controllers\QuestionaireController@edit_questionaire2');
-Route::put('/edit_questionaire3','App\http\Controllers\QuestionaireController@edit_questionaire3');
-Route::put('/edit_questionaire4','App\http\Controllers\QuestionaireController@edit_questionaire4');
+Route::post('/get_questionaire','QuestionaireController@get_questionaire');
+Route::put('/edit_questionaire1','QuestionaireController@edit_questionaire1');
+Route::put('/edit_questionaire2','QuestionaireController@edit_questionaire2');
+Route::put('/edit_questionaire3','QuestionaireController@edit_questionaire3');
+Route::put('/edit_questionaire4','QuestionaireController@edit_questionaire4');
 
-Route::post('/submit_form','App\http\Controllers\ResultsController@submit_form');
-Route::post('/verify_evaluate','App\http\Controllers\ResultsController@verify_evaluate');
-Route::post('/get_all_results','App\http\Controllers\ResultsController@get_all_results');
-Route::post('/get_all_results2','App\http\Controllers\ResultsController@get_all_results2'); // check
-Route::post('/goto_overall','App\http\Controllers\ResultsController@goto_overall'); // check
-Route::post('/get_all_overall','App\http\Controllers\ResultsController@get_all_overall');
-Route::post('/counting_data','App\http\Controllers\ResultsController@counting_data');
+Route::post('/submit_form','ResultsController@submit_form');
+Route::post('/verify_evaluate','ResultsController@verify_evaluate');
+Route::post('/get_all_results','ResultsController@get_all_results');
+Route::post('/get_all_results2','ResultsController@get_all_results2'); // check
+Route::post('/goto_overall','ResultsController@goto_overall'); // check
+Route::post('/get_all_overall','ResultsController@get_all_overall');
+Route::post('/counting_data','ResultsController@counting_data');
 
-Route::post('/get_schedule','App\http\Controllers\ScheduleController@get_schedule'); // check
-Route::put('/update_schedule','App\http\Controllers\ScheduleController@update_schedule');
-Route::put('/change_sem','App\http\Controllers\ScheduleController@change_sem');
+Route::post('/get_schedule','ScheduleController@get_schedule'); // check
+Route::put('/update_schedule','ScheduleController@update_schedule');
+Route::put('/change_sem','ScheduleController@change_sem');
 
-Route::post('/user_login','App\http\Controllers\UsersController@user_login');//check
-Route::post('/get_all_users','App\http\Controllers\UsersController@get_all_users'); //check
-Route::post('/get_all_users2','App\http\Controllers\UsersController@get_all_users2');
-Route::post('/add_campus','App\http\Controllers\UsersController@add_campus');
+Route::post('/user_login','UsersController@user_login');//check
+Route::post('/get_all_users','UsersController@get_all_users'); //check
+Route::post('/get_all_users2','UsersController@get_all_users2');
+Route::post('/add_campus','UsersController@add_campus');
 
-Route::post('/get_subject_loading','App\http\Controllers\SubjectLoadingController@get_subject_loading');//check
-Route::post('/add_subject_loaded','App\http\Controllers\SubjectLoadingController@add_subject_loaded');//check
-Route::post('/select_course_year','App\http\Controllers\SubjectLoadingController@select_course_year');
-Route::post('/get_student_subject_loading','App\http\Controllers\SubjectLoadingController@get_student_subject_loading');//check
+Route::post('/get_subject_loading','SubjectLoadingController@get_subject_loading');//check
+Route::post('/add_subject_loaded','SubjectLoadingController@add_subject_loaded');//check
+Route::post('/select_course_year','SubjectLoadingController@select_course_year');
+Route::post('/get_student_subject_loading','SubjectLoadingController@get_student_subject_loading');//check
 
 
-Route::post('/qrscanner','App\http\Controllers\ScannerController@qrscanner'); //check
+Route::post('/qrscanner','ScannerController@qrscanner'); //check
 
