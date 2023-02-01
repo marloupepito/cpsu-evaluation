86a31a528dddcf72a674e7b46186fd1c7795319a
marloupepito <marloupepito07@gmail.com>
2023-02-01 21:49:13 +0800

    da lmdla wdlawjd lajdwa

--- database/migrations/2023_01_28_061842_create_students_table.php
+++ database/migrations/2023_01_28_061842_create_students_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('id_number')->nullable();
+            $table->string('id_number')->nullable();
             $table->bigInteger('campusid')->nullable();
             $table->string('campus')->nullable();
             $table->string('name')->nullable();
